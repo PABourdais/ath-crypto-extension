c2b88e715ae91446119fe3f15abc3ec8fe67f3fa
PABourdais <pierre.alex.bourdais@gmail.com>
2021-11-16 10:44:02 +0100

    add periodic alarm

--- background.js
+++ background.js
@@ -1,6 +1,16 @@
 let cryptoList = ['BTC','ETH'];
 
+chrome.alarms.onAlarm.addListener(a => {
+  console.log(`cryptoList: ${cryptoList}`, a);
+});
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ cryptoList });
-  console.log('Default coin is set to BTC and ETH', `cryptoList: ${cryptoList}`);
+
+  chrome.alarms.get('alarm', a => {
+    if (!a) {
+      chrome.alarms.create('alarm ', {periodInMinutes: 1});
+    }
+  });
+
 });
