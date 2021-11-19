edb0115440d6cbbcb4fc348c978c95b2a8c4f6f8
PABourdais <pierre.alex.bourdais@gmail.com>
2021-11-19 16:16:11 +0100

    improve popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo_github from './images/logo_github.png';
 import wrench from './images/wrench.png';
+import trash from './images/trash.png';
 
 import './App.css';
 
@@ -12,11 +13,11 @@ export const App = () => {
         <li className="List-content">
             <a target="_blank" href={"https://www.google.com/search?q=" + crypto}>{crypto}</a>
             <button
-                style={{backgroundColor: 'red', width: 20, height: 20}}
                 onClick={() => {
                     deleteCrytpoList(index);
                 }}
-            >X
+            >
+                <img alt="github" src={trash}/>
             </button>
         </li>
     );
@@ -43,6 +44,7 @@ export const App = () => {
     return (
         <div className="App">
             <header className="App-header">
+                <h1>ATH crypto extension</h1>
                 <form onSubmit={handleSubmit}>
                     <input
                         value={input}
@@ -51,14 +53,14 @@ export const App = () => {
                         placeholder='Add to fav...'
                         id="crypto-search"
                     />
-                    <button onClick={handleSubmit} value="Envoyer">
+                    <button onClick={handleSubmit} value="Envoyer" className="Button-form">
                         Add
                     </button>
                 </form>
                 <ul className="List">{listItems}</ul>
                 <div className="bottom">
-                    It's {new Date().toLocaleTimeString()}.
                     <a href="" id="options" className="imageLink"><img alt="option" src={wrench}/></a>
+                    {new Date().toLocaleTimeString()}
                     <a href="https://github.com/PABourdais/ath-crypto-extension" id="github" className="imageLink"
                        target="_blank"><img alt="github" src={logo_github}/></a>
                 </div>
