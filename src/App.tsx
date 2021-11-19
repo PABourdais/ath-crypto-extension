71ee9666377fa4c3be4736fdbccb267256282483
PABourdais <pierre.alex.bourdais@gmail.com>
2021-11-19 11:50:18 +0100

    add list crypto

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,35 @@ import React, { useState } from 'react';
 import './App.css';
 
 export const App = () => {
-  const [crytpoList, setCrytpoList] = useState<string[]>(['BTC', 'ETH', 'EGLD', 'ADA']);
+    const [crytpoList, setCrytpoList] = useState<string[]>(['BTC', 'ETH', 'EGLD', 'ADA']);
 
-  const listItems = crytpoList.map((crypto) =>
-      <li>{crypto}</li>
-  );
+    const listItems = crytpoList.map((crypto, index) =>
+        <li className="List-content">
+            <a target="_blank" href={"https://www.google.com/search?q=" + crypto}>{crypto}</a>
 
-  return (
-      <div className="App">
-        <header className="App-header">
-          <ul>{listItems}</ul>
-          <p>Il est {new Date().toLocaleTimeString()}.</p>
-        </header>
-      </div>
-  );
+            <button
+                style={{backgroundColor: 'red', width: 20, height: 20}}
+                onClick={() => {
+                deleteCrytpoList(index);
+                }}
+            >X</button>
+        </li>
+    );
+
+    /* delete item in crytpoList*/
+    const deleteCrytpoList = (cryptoIndex: number) => {
+        const newCrytpoList = crytpoList.filter((_, index) => index !== cryptoIndex);
+        setCrytpoList(newCrytpoList);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ul className="List">{listItems}</ul>
+                <p>Il est {new Date().toLocaleTimeString()}.</p>
+            </header>
+        </div>
+    );
 };
 
 export default App
