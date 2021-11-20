b0c7eca31696f47bfecd03435e9f4728d326aef3
PABourdais <pierre.alex.bourdais@gmail.com>
2021-11-20 16:23:32 +0100

    add coin type

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,23 @@ export interface ChromeMessage {
     from: Sender,
     message: any
 }
+
+export interface crypto {
+    id: string;
+    icon: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    price: number;
+    priceBtc: number;
+    volume: number;
+    marketCap: number;
+    availableSupply: number;
+    totalSupply : number;
+    priceChange1h : number;
+    priceChange1d : number;
+    priceChange1w : number;
+    websiteUrl : string;
+    twitterUrl : string;
+    exp : string[];
+}
